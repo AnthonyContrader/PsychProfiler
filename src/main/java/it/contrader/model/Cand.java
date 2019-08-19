54482d9e9805901a7fa16331e62a191ec8f18230
superandrew90 <giuffridaandrea90@hotmail.it>
2019-08-19 11:46:04 +0200

    Update model

---
+++ src/main/java/it/contrader/model/Cand.java
@@ -0,0 +1,35 @@
+package it.contrader.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Cand {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+    @Column(unique=true)
+    private String name;
+	
+	private String surname;
+	
+	private int age;
+	
+	private String experience;
+	@OneToOne
+	private User user;
+
+}
---
+++ src/main/java/it/contrader/model/Job.java
@@ -0,0 +1,27 @@
+package it.contrader.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Job {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(unique=true)
+	private String name;
+	
+	private String description;
+}
