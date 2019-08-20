b904fb482c2f2bc78b4c1ae5509e995bc13dd600
Paranoidmc <p.marasca89@gmail.com>
2019-08-20 17:02:15 +0200

    SpringAll

--- src/main/java/it/contrader/converter/ConverterAnswer.java
+++ src/main/java/it/contrader/converter/ConverterAnswer.java
@@ -1,53 +1,32 @@
 package it.contrader.converter;
-import java.util.ArrayList;
-import java.util.List;
+
+import org.springframework.stereotype.Component;
 
 import it.contrader.dto.AnswerDTO;
+
 import it.contrader.model.Answer;
 
-public class ConverterAnswer {
-	public static AnswerDTO toDTO(Answer answer) {
-		AnswerDTO answerDTO = null;
-		if (answer != null) {
-			answerDTO = new AnswerDTO();
-			answerDTO.setId(answer.getId());
-			answerDTO.setCand(answer.getCand());
-			answerDTO.setQuest(answer.getQuest());
-			answerDTO.setAns(answer.getAns());
-			
-		}
-		return answerDTO;
-	}
-	public static Answer toEntity(AnswerDTO answerDTO) {
-		Answer answer= null;
+@Component
+public class ConverterAnswer extends AbstractConverter<Answer, AnswerDTO> {
+
+	@Override
+	public Answer toEntity(AnswerDTO answerDTO) {
+		Answer answer = null;
 		if(answerDTO != null) {
-			 answer= new Answer();
-			 answer.setId(answerDTO.getId());
-			 answer.setCand(answerDTO.getCand());
-			 answer.setQuest(answerDTO.getQuest());
+			answer = new Answer(answerDTO.getId(), answerDTO.getCand(),answerDTO.getQuest(),answerDTO.getAns());
 		}
 		return answer;
 	}
-	public static List<AnswerDTO> toListDTO(List<Answer> list)
-	{
-		List<AnswerDTO> listAnswerDTO = new ArrayList<>();
-		if(!list.isEmpty()) {
-			for(AnswerDTO answerDTO : listAnswerDTO) {
-				list.add(ConverterAnswer.toEntity(answerDTO));
-			}
-			
+
+	@Override
+	public AnswerDTO toDTO(Answer answer) {
+		AnswerDTO answerDTO = null;
+		if (answer != null) {
+			answerDTO = new AnswerDTO(answer.getId(), answer.getCand(), answer.getQuest(), answer.getAns());
+
 		}
-		return listAnswerDTO;
+		return answerDTO;
 	}
-	public static List<Answer> toListEntity(List<AnswerDTO> listAnswerDTO){
-		List<Answer> list = new ArrayList<>();
-		if(!listAnswerDTO.isEmpty()) {
-			for(AnswerDTO answerDTO : listAnswerDTO) {
-				list.add(ConverterAnswer.toEntity(answerDTO));
-			}
-			
-		}
-		return list;
-	}
- 
+
+
 }
--- src/main/java/it/contrader/converter/ConverterQuest.java
+++ src/main/java/it/contrader/converter/ConverterQuest.java
@@ -1,60 +1,32 @@
 package it.contrader.converter;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.stereotype.Component;
 
 import it.contrader.dto.QuestDTO;
+
 import it.contrader.model.Quest;
 
-public class ConverterQuest {
-	public static QuestDTO toDTO(Quest quest) {
-		QuestDTO questDTO = null;
-		if(quest != null) {
-			questDTO= new QuestDTO();
-			questDTO.setId(quest.getId());
-			questDTO.setArgs(quest.getArgs());
-			questDTO.setQuest(quest.getQuest());
-			questDTO.setAns1(quest.getAns1());
-			questDTO.setAns2(quest.getAns2());
-			questDTO.setAns3(quest.getAns3());
-			questDTO.setAns4(quest.getAns4());
-		}
-		return questDTO;
-	}
-	public static Quest toEntity(QuestDTO questDTO) {
+@Component
+public class ConverterQuest extends AbstractConverter<Quest, QuestDTO> {
+
+	@Override
+	public Quest toEntity(QuestDTO questDTO) {
 		Quest quest = null;
-		if(questDTO != null) {
-		quest = new Quest();
-		quest.setId(questDTO.getId());
-		quest.setArgs(questDTO.getArgs());
-		quest.setQuest(questDTO.getQuest());
-		quest.setAns1(questDTO.getAns1());
-		quest.setAns2(questDTO.getAns2());
-		quest.setAns3(questDTO.getAns3());
-		quest.setAns4(questDTO.getAns4());
+		if (questDTO != null) {
+			quest = new Quest(questDTO.getId(), questDTO.getArgs(), questDTO.getQuest(), questDTO.getAns1(),
+							questDTO.getAns2(),questDTO.getAns3(),questDTO.getAns4());
 		}
 		return quest;
 	}
 
-	public static List<QuestDTO> toListDTO(List<Quest> list) {
-		List<QuestDTO> listQuestDTO = new ArrayList<>();
-		if (!list.isEmpty()) {
-			for (Quest Quest : list) {
-				listQuestDTO.add(ConverterQuest.toDTO(Quest));
-			}
-		}
-		return listQuestDTO;
-	}
+	@Override
+	public QuestDTO toDTO(Quest quest) {
+		QuestDTO questDTO = null;
+		if (quest != null) {
+			questDTO = new QuestDTO(quest.getId(), quest.getArgs(), quest.getQuest(), quest.getAns1(),
+					quest.getAns2(),quest.getAns3(),quest.getAns4());
 
-	public static List<Quest> toListEntity(List<QuestDTO> listQuestDTO) {
-		List<Quest> list = new ArrayList<>();
-		if (!listQuestDTO.isEmpty()) {
-			for (QuestDTO questDTO : listQuestDTO) {
-				list.add(ConverterQuest.toEntity(questDTO));
-			}
 		}
-		return list;
+		return questDTO;
 	}
 }
-
-
