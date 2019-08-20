d32fe2533f62ac8afc791e85c038d8b913f54f7f
superandrew90 <giuffridaandrea90@hotmail.it>
2019-08-20 16:58:42 +0200

    Update CONVERTER1

--- src/main/java/it/contrader/converter/ConverterCand.java
+++ src/main/java/it/contrader/converter/ConverterCand.java
@@ -1,56 +1,29 @@
 package it.contrader.converter;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.stereotype.Component;
 
 import it.contrader.dto.CandDTO;
 import it.contrader.model.Cand;
 
-public class ConverterCand {
-	public static CandDTO toDTO(Cand cand) {
-		CandDTO candDTO = null;
-		if (cand != null) {
-			candDTO = new CandDTO();
-			candDTO.setId(cand.getId());
-			candDTO.setName(cand.getName());
-			candDTO.setAge(cand.getAge());
-			candDTO.setExperience(cand.getExperience());
-			candDTO.setUser(cand.getUser());
+@Component
+public class ConverterCand extends AbstractConverter<Cand,CandDTO> {
 
-		}
-		return candDTO;
-	}
-
-	public static Cand toEntity(CandDTO candDTO) {
+	@Override
+	public Cand toEntity(CandDTO candDTO) {
 		Cand cand = null;
 		if (candDTO != null) {
-			cand = new Cand();
-			cand.setId(candDTO.getId());
-			cand.setName(candDTO.getName());
-			cand.setAge(candDTO.getAge());
-			cand.setExperience(candDTO.getExperience());
-			cand.setUser(candDTO.getUser());
+			cand = new Cand(candDTO.getId(), candDTO.getName(), candDTO.getSurname(), candDTO.getAge(), candDTO.getExperience(), candDTO.getUser());
 		}
 		return cand;
 	}
 
-	public static List<CandDTO> toListDTO(List<Cand> list) {
-		List<CandDTO> listCandDTO = new ArrayList<>();
-		if (!list.isEmpty()) {
-			for (Cand cand : list) {
-				listCandDTO.add(ConverterCand.toDTO(cand));
-			}
-		}
-		return listCandDTO;
-	}
+	@Override
+	public CandDTO toDTO(Cand cand) {
+		CandDTO candDTO = null;
+		if (cand != null) {
+			candDTO = new CandDTO(cand.getId(), cand.getName(), cand.getSurname(), cand.getAge(), cand.getExperience(), cand.getUser());
 
-	public static List<Cand> toListEntity(List<CandDTO> listCandDTO) {
-		List<Cand> list = new ArrayList<>();
-		if (!listCandDTO.isEmpty()) {
-			for (CandDTO candDTO : listCandDTO) {
-				list.add(ConverterCand.toEntity(candDTO));
-			}
 		}
-		return list;
+		return candDTO;
 	}
 }
--- src/main/java/it/contrader/converter/ConverterJob.java
+++ src/main/java/it/contrader/converter/ConverterJob.java
@@ -1,53 +1,29 @@
 package it.contrader.converter;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.stereotype.Component;
 
 import it.contrader.dto.JobDTO;
 import it.contrader.model.Job;
 
-public class ConverterJob {
-	public static JobDTO toDTO(Job job) {
-		JobDTO jobDTO = null;
-		if (job != null) {
-			jobDTO = new JobDTO();
-			jobDTO.setId(job.getId());
-			jobDTO.setName(job.getName());
-			jobDTO.setDescription(job.getDescription());
-		}
-		return jobDTO;
-	}
+@Component
+public class ConverterJob extends AbstractConverter<Job, JobDTO> {
 
-	public static Job toEntity(JobDTO jobDTO) {
+	@Override
+	public Job toEntity(JobDTO jobDTO) {
 		Job job = null;
 		if (jobDTO != null) {
-			job = new Job();
-			job.setId(jobDTO.getId());
-			job.setName(jobDTO.getName());
-			job.setDescription(jobDTO.getDescription());
+			job = new Job(jobDTO.getId(), jobDTO.getName(), jobDTO.getDescription());
 		}
 		return job;
 	}
 
-	public static List<JobDTO> toListDTO(List<Job> list) {
-		List<JobDTO> listJobDTO = new ArrayList<>();
-		if (!list.isEmpty()) {
-			for (Job job : list) {
-				listJobDTO.add(ConverterJob.toDTO(job));
-			}
-		}
-		return listJobDTO;
-	}
+	@Override
+	public JobDTO toDTO(Job job) {
+		JobDTO jobDTO = null;
+		if (job != null) {
+			jobDTO = new JobDTO(job.getId(), job.getName(), job.getDescription());
 
-	public static List<Job> toListEntity(List<JobDTO> listJobDTO) {
-		List<Job> list = new ArrayList<>();
-		if (!listJobDTO.isEmpty()) {
-			for (JobDTO jobDTO : listJobDTO) {
-				list.add(ConverterJob.toEntity(jobDTO));
-			}
 		}
-		return list;
+		return jobDTO;
 	}
 }
-
-
